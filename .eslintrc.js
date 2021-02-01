34e37da2c593f8b0d6469620f65599b17c727ef4
Eduardo Fermiano Rocha <Eduardo.Rocha@sky.com.br>
2021-02-01 15:26:13 -0300

    setup lint, editorconfig and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "standard",
+    "prettier",
+    "prettier/standard",
+    "prettier/react",
+  ],
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    "import/prefer-default-export": "off",
+  },
+};
