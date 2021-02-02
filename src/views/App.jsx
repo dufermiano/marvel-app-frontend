de54a71c7bb519c114117680b4be5becae71b4c8
Eduardo Fermiano Rocha <Eduardo.Rocha@sky.com.br>
2021-02-02 17:42:23 -0300

    setting up app rendering

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
 
-import Menu from "../components/layout/Menu";
-import Content from "../components/layout/Content";
+import Menu from '../components/layout/Menu';
+import Content from '../components/layout/Content';
 
 const App = (props) => (
   <div className="App">
